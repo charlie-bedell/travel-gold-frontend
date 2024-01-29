fad043e1324094cfc4f47c2d8817d7eabbc948d6
Charles Bedell <charlesbedell5@gmail.com>
2024-01-29 09:21:44 -0500

    updating post reqs and add console logs for debugging

--- src/services/authService.js
+++ src/services/authService.js
@@ -9,12 +9,15 @@ function getUser() {
 
 async function signup(user) {
   try {
-    const res = await axios.post(`${BASE_URL}/signup`,JSON.stringify(user));
+    const res = await axios.post(`${BASE_URL}/signup`,JSON.stringify(user),{ headers: {"Content-Type": "application/json"}});
     // const res = await fetch(`${BASE_URL}/signup`, {
     //   method: "POST",
     //   headers: { "Content-Type": "application/json" },
     //   body: JSON.stringify(user),
     // });
+    console.log("res: ", res);
+    console.log("res data: ", res.data());
+    console.log("res json: ", res.json());
     const json = await res.json();
     if (json.token) {
       tokenService.setToken(json.token);
@@ -35,8 +38,11 @@ async function login(credentials) {
   //   headers: { "Content-Type": "application/json" },
   //   body: JSON.stringify(credentials),
   // });
-
-  const res = await axios.post(`${BASE_URL}/login`,JSON.stringify(credentials));
+  
+  const res = await axios.post(`${BASE_URL}/login`, JSON.stringify(credentials), { headers: {"Content-Type": "application/json"}});
+  console.log("res: ", res);
+  console.log("res data: ", res.data());
+  console.log("res json: ", res.json());
   const json = await res.json();
 
   if (json.token) {
